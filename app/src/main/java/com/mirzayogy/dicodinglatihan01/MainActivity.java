e160324876fbbb6e19340c86cd40c8e577acd7ee
mirzayogy <mirza.yogy@gmail.com>
2018-06-03 14:04:19 +0800

    deklarasi variable komponen untuk java

--- app/src/main/java/com/mirzayogy/dicodinglatihan01/MainActivity.java
+++ app/src/main/java/com/mirzayogy/dicodinglatihan01/MainActivity.java
@@ -3,8 +3,15 @@ package com.mirzayogy.dicodinglatihan01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText edtWidth, edtHeight, edtLength;
+    private Button btnCalculate;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        
+
     }
 }
